583c815012d2f642665be2c2d8729c50ffdab0e1
unknown <joao.silvaneto@bandtec.com.br>
2024-06-21 07:47:22 -0300

    Refatoração do codigo e conexão com o banco de dados local MySQL

---
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,65 @@
+package br.com.erudio.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public List<Person> findAll() {
+		
+		logger.info("Buscando todas as pessoas!");
+		
+		return repository.findAll();
+	}
+	
+
+	public Person findById(Long id) {
+		
+		logger.info("Buscando uma pessoa!");
+		
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for thin ID"));
+	}
+	
+	public Person create(Person person) {
+		
+		logger.info("Criando uma pessoa!");
+		
+		return repository.save(person);
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("Atualizando uma pessoa!");
+		
+		Person entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for thin ID"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(person);
+	}
+	
+	public void delete(Long id) {
+		
+		logger.info("Deletando uma pessoa!");
+		
+		Person entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for thin ID"));
+		
+		repository.delete(entity);
+	}
+}
